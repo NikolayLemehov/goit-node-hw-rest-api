bf71e3bb8ef67a7b284d18abfc902c4d571d763a
Nikolay_Liemiekhov <nicklemster@gmail.com>
2023-01-02 15:38:30 +0200

    fix fake

--- middlewares/validationMiddleware.js
+++ middlewares/validationMiddleware.js
@@ -17,7 +17,7 @@ const checkContactData = (req, res, next) => {
   next();
 };
 
-const addContact = checkContactData;
+const addContact = checkContactDatas;
 
 const updateContact = checkContactData;
 
